5f7009f5222858ca43b1ed46cb63e44062de5aae
risa-skmt <skmt.risa@gmail.com>
2023-05-15 18:54:41 +0900

    port8000で'hello world!'表示

---
+++ phrase-keeper/backend/index.js
@@ -0,0 +1,54 @@
+//requireでexpressモジュールを読み込む
+const express = require('express');
+//mysql2の読み込む
+const mysql = require('mysql2');
+//expressモジュールを実体化して、定数appに代入
+const app = express();
+//ポート番号を指定
+const port = 8000;
+
+
+//mysqlと接続するための設定
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'my00sql0first',
+  database: 'phrases'
+});
+
+//'/'パスにGET要求があった際に実行する処理
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+//'/api'パスにGET要求があった際に実行する処理
+// app.get("/api", (req, res) => {
+//     res.json({message: "Hello World"});
+//   });
+
+
+  app.get('/api', (req, res) => {
+    // /apiにアクセスした際に、MySQLに対して行う処理
+    connection.query(
+        //usersテーブルからデータを取得する処理
+        'SELECT * FROM users',
+        function(err, results, fields){
+            if(err){
+                console.log('接続エラー');
+                throw err;
+            }
+            //res.json({message: results[0].name});
+            res.json({data: results}); // 全てのデータを返す場合
+        }
+    )
+});
+
+
+
+//8000ポートでlisten
+app.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
+
+//npm run start-nodeでサーバー起動
---
+++ phrase-keeper/frontend/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+//1．useStateとuseEffectをインポート
+import { useState,useEffect } from 'react';
+
+
+function App() {
+  //useStateの初期値（空）を設定
+  // const [message, setMessage] = useState('');
+//dataステートを初期化（？）
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    //fetchでバックエンドExpressのサーバーを指定
+    fetch('/api')
+      //レスポンスをjsonとして受け取りjsオブジェクトを生成
+      .then((res) => res.json())
+      //生成したjsオブジェクトをdataに代入
+      //data.messageで取り出したデータをuseStateに保存
+      .then((data) => setData(data.data))
+      //追加
+      .catch((error) => console.log(error));
+  },[])
+
+
+  return (
+    <div className="App">
+      <h1>フロントエンド</h1>
+      {/* useStateに保存した値を表示
+      <p>{ message }</p> */}
+     {/* バックエンドから取得した全てのデータをdataステートに保存し、map関数で表示 */}
+       {data.map((item) => (
+      <p key={item.id}>{item.name}</p>
+    ))}
+    </div>
+  );
+}
+
+export default App;
