cc4820242f27867bfcef22cc37e1c56595a960ac
risa-skmt <skmt.risa@gmail.com>
2023-05-17 19:16:11 +0900

    componentsを呼び出して反映

--- phrase_keeper/frontend/src/App.js
+++ phrase_keeper/frontend/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Title  from "./components/Title";
+import InputForm from './components/InputForm';
+import List from './components/List';
+import DB from './components/DB';
 
 //1．useStateとuseEffectをインポート
 import { useState,useEffect } from 'react';
+
 
 
 function App() {
@@ -14,31 +19,38 @@ function App() {
 //setPhraseListは状態を変更するための変数 //型は配列にしておく
 const [phraseList, setPhraseList] = useState([]);
 
-  useEffect(() => {
-    //fetchでバックエンドExpressのサーバーを指定
-    fetch('/api')
-      //レスポンスをjsonとして受け取りjsオブジェクトを生成
-      .then((res) => res.json())
-      //生成したjsオブジェクトをdataに代入
-      //data.messageで取り出したデータをuseStateに保存
-      .then((data) => setData(data.data))
-      //追加
-      .catch((error) => console.log(error));
-  },[])
+
+//消す？？
+  // useEffect(() => {
+  //   //fetchでバックエンドExpressのサーバーを指定
+  //   fetch('/api')
+  //     //レスポンスをjsonとして受け取りjsオブジェクトを生成
+  //     .then((res) => res.json())
+  //     //生成したjsオブジェクトをdataに代入
+  //     //data.messageで取り出したデータをuseStateに保存
+  //     .then((data) => setData(data.data))
+  //     //追加
+  //     .catch((error) => console.log(error));
+  // },[])
 
 
   return (
     <div className="App">
-      <h1>Phrase Keeper</h1>
-      
+      <Title />
+      <InputForm phraseList={phraseList} setPhraseList={setPhraseList}/>
+      <DB data={data} setData={setData}/> 
+      <List phraseList={phraseList} setPhraseList={setPhraseList}/>
+     
       
       {/* useStateに保存した値を表示
       <p>{ message }</p> */}
      {/* バックエンドから取得した全てのデータをdataステートに保存し、map関数で表示 */}
-       {data.map((item) => (
+
+{/* 消す？？ */}
+       {/* {data.map((item) => (
       <p key={item.id} class="phrase-container">{item.phrase}</p>
     ))}
-    
+     */}
     </div>
   );
 }
